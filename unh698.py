7e4a72b5de8cc97b6045f194220dc6242d54d2c3
flochner <flochner@gmail.com>
2017-03-28 12:18:09 -0400

    clean up branches

--- unh698.py
+++ unh698.py
@@ -8,10 +8,3 @@ def hello_world_fml():
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
-
-# class app:
-#     def test_client():
-#         pass
-
-#     def get():
-#         pass
