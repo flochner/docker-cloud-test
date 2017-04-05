d00fa2478f19cf67709253be5ed14be314ec96d7
flochner <flochner@gmail.com>
2017-04-05 13:07:13 -0400

    added \n

--- unh698.py
+++ unh698.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return 'UNH698 Website'
+    return 'UNH698 Website\n'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
