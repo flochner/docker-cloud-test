6707fa419b3a9d8abac679a1a8240d4b80fc94a3
flochner <flochner@gmail.com>
2017-04-17 19:45:32 -0400

    more website fixes

--- unh698.py
+++ unh698.py
@@ -1,14 +1,15 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
+#@app.route('/<name>')
 def main():
-    return 'UNH698 Website\nSpeed Racer\n'
+    return render_template('hello.html', name=name)
     
 @app.route('/SpeedRacer')
-def main():
+def speed():
     return 'Speed Racer\n'
     
-
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
