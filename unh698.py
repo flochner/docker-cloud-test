510abcf97811a13c9494cbc6242823bb21943b84
flochner <flochner@gmail.com>
2017-04-17 19:13:09 -0400

    updated main website and tests

--- unh698.py
+++ unh698.py
@@ -3,7 +3,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return 'UNH698 Website\n'
+    return 'UNH698 Website\nSpeed Racer\n'
+    
+@app.route('/SpeedRacer')
+def main():
+    return 'Speed Racer\n'
+    
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
