1a2ad3ea6d8b7e3c15e553613a7071bd3d66c54d
flochner <flochner@gmail.com>
2017-04-21 09:52:12 -0400

    added vars and removed unneeded -v

--- run-playbook.sh
+++ run-playbook.sh
@@ -1,3 +1,3 @@
-ansible-playbook ansible/configure-host.yml -v -e "student_username=flochner"
-ansible-playbook ansible/deploy-website-staging.yml -v
-ansible-playbook ansible/deploy-website-production.yml -v
+ansible-playbook ansible/configure-host.yml -e "student_username=flochner"
+ansible-playbook ansible/deploy-website-staging.yml
+ansible-playbook ansible/deploy-website-production.yml
