673b3b5872b2fccbf0a15881948ce49a4023f6b5
flochner <flochner@gmail.com>
2017-04-25 01:54:39 -0400

    working flask templates

--- unh698.py
+++ unh698.py
@@ -1,15 +1,27 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def main():
+def home():
     return render_template('main.html')
     
 @app.route('/SpeedRacer')
-def speed():
-    return render_template('SpeedRacer/main.html')
+def fakeDatabase():
+    f = (
+         {'name':'Speed', 'castMember':'18-year-old professional racer'},
+         {'name':'Spritle', 'castMember':'Speed\'s little brother'},
+         {'name':'Pops', 'castMember':'Speed\'s father'},
+         {'name':'Mrs. Racer', 'castMember':'Speed\'s mother'},
+        )
+    o = (
+         {'name':'Trixie', 'castMember':'Speed\'s girlfriend'},
+         {'name':'Racer X', 'castMember':'Masked racer, and secretly, Speed\'s older brother'},
+         {'name':'Chim-Chim', 'castMember':'Spritle\'s chimpanzee'},
+         {'name':'Sparky', 'castMember':'Mach 5 mechanic'},
+        )
+    return render_template('SpeedRacer/main.html', family=f, others=o)
     
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.run(host='0.0.0.0')
  
